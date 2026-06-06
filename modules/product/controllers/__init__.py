fb6a067075acce561a782c8cced9d647ac5ad7b6
Jsouza181gh <emanuelrodrigues010@gmail.com>
2026-06-05 23:49:38 -0300

    feat(product-description): add product description controllers and dependences

--- modules/product/controllers/__init__.py
+++ modules/product/controllers/__init__.py
@@ -1,8 +1,10 @@
+from .product_description_section import router as description_router
 from .product_category import router as category_router
 from .product_image import router as image_router
 from .product import router as product_router
 
 routers = [
+    description_router,
     category_router,
     product_router,
     image_router
---
+++ modules/product/controllers/dependences/product_description_section.py
@@ -0,0 +1,23 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+
+from infrastructure.database.session import get_database
+from ...repositories import ProductDescriptionSectionRepository, ProductRepository
+from ...services import ProductDescriptionSectionService
+from .product import get_product_repository
+
+def get_product_description_section_repository(db: Annotated[AsyncSession, Depends(get_database)]):
+    return ProductDescriptionSectionRepository(db)
+
+
+def get_product_description_section_service(
+    description_repository: Annotated[ProductDescriptionSectionRepository, Depends(get_product_description_section_repository)],
+    product_repository: Annotated[ProductRepository, Depends(get_product_repository)]
+):
+    return ProductDescriptionSectionService(description_repository, product_repository)
+
+ProductDescriptionSectionDependences = Annotated[
+    ProductDescriptionSectionService,
+    Depends(get_product_description_section_service)
+]
